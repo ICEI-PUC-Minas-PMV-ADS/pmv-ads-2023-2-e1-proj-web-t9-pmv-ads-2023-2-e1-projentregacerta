b16161cc631498447607a89ae9c0b024546e1f0c
Rodrigo Araujo Ribeiro <144185306+1806rodrigo@users.noreply.github.com>
2023-11-20 20:37:56 -0300

    Update script_log.js

--- documentos/ENTREGA CERTA PRINCIPAL/script_log.js
+++ documentos/ENTREGA CERTA PRINCIPAL/script_log.js
@@ -1,33 +1,39 @@
-const usuario8 = localStorage.getItem("usuario1");
-const password8 = localStorage.getItem("password1");
-const selecao8 = localStorage.getItem("selecao1");
-
 var elem0 = document.getElementById('btnLogin');
 elem0.addEventListener('click', function (fLogin) {
     const usuario0 = document.getElementById('usuario').value;
     const password0 = document.getElementById('password').value;
-    if (usuario0 == "" || password0 == "") {
-        alert("Os campos e-mail e senha são obrigatórios");
+    let usuarioValid = {
+        usuarioLog: '',
+        passwordLog: '',
+        selecaoLog: ''
     }
-    else {
-        if (usuario0 == usuario8 && password0 == password8){
-            alert("Login com sucesso");
-    
-            if (selecao8 == 0)
-            {
-                location.href = "homeEmpregador.html";
+
+    listaUsuario = JSON.parse(localStorage.getItem('listaUsuario'));
+    listaUsuario.forEach((item) => {
+        if (usuario0 == item.usuarioCad && password0 == item.passwordCad) {
+            usuarioValid = {
+                usuarioLog: item.usuarioCad,
+                passwordLog: item.passwordCad,
+                selecaoLog: item.selecaoCad
             }
-            if (selecao8 == 1)
-            {
-                location.href = "homeMotoboy.html";
-            }
-       
         }
-        else{
-            alert("Nome de usuário e senha não conferem");
-            }
+
+
+
+    })
+
+    if (usuario0 == usuarioValid.usuarioLog && password0 == usuarioValid.passwordLog) {
+        alert("Login efetuado com sucesso");
+
+        if (usuarioValid.selecaoLog == 0) {
+            location.href = "homeEmpregador.html";
+        }
+        if (usuarioValid.selecaoLog == 1) {
+            location.href = "homeMotoboy.html";
+        }
+
+
+    } else {
+        alert("Nome de usuário e senha não conferem");
     }
 }, false);
-
-//alert(selecaoMotoboy);
-//alert(selecaoEmpregador);
